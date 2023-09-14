92b664031460d33d725b2c850825fc58beef0172
favvy04 <funmilolaadebiyi52@gmail.com>
2023-09-14 23:59:59 +0100

    added new feature

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
+const emailField = document.getElementById("email-field");
 const submitBtn = document.querySelector(".submit-btn");
-const input = document.getElementById('email');
-const inputValue = input.value;
-const placeholderRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-const signupInfo= document.querySelector(".container");
-const sucessMessage = document.querySelector('.container2');
+const inputValue = emailField.value;
+const errorMessage = document.getElementById('email-error');
+const emailFormat = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+// const signupInfo= document.querySelector(".container");
+// const sucessMessage = document.querySelector('.container2');
 submitBtn.addEventListener('click',function(){
-    if(placeholderRegex.test(inputValue)){
+    if(emailFormat===inputValue){
       window.location.href = "index..html";
+      errorMessage.innerHTML = "";
     } else {
-        alert('invalid email');
-    };
-
+      errorMessage.innerHTML = "valid email required";
+      emailField.style.border = "1px solid hsl(4, 100%, 67%)";
+      submitBtn.style.backgroundColor = "hsl(234, 29%, 20%)";
+      submitBtn.style.border = "1px solid hsl(234, 29%, 20%)";
+    } 
 });
